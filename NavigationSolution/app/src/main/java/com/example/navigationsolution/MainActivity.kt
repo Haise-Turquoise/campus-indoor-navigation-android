dd163c5128f8795f4e211392267504fbfd853162
Daniel Cao <xxx@uwaterloo.ca>
2025-02-05 20:38:45 -0500

    Initial commit

---
+++ NavigationSolution/app/src/main/java/com/example/navigationsolution/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.navigationsolution
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationsolution.ui.theme.NavigationSolutionTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface() {
+                    OpeningScreen()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun OpeningScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.uwlogo),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .height(150.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(10.dp))
+        )
+
+        Text("Pathfinder", fontSize = 30.sp)
+
+        var buttonText = remember { mutableStateOf("Go")}
+        Button(onClick = { buttonText.value = "Transition"}) {
+            Text(text = buttonText.value)
+        }
+    }
+
+
+}
