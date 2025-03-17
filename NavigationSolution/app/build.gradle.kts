e98fb22cb7c68112d81076e155bfa2214679a4fe
ted-he <xxx@uwaterloo.ca>
2025-03-17 13:47:24 -0400

    add unit test

--- NavigationSolution/app/build.gradle.kts
+++ NavigationSolution/app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
