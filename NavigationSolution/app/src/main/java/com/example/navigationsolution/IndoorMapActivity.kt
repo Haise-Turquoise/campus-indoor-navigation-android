c814d3fd273fd3580590f84e569273f9a8afcaeb
Daniel Cao <xxx@uwaterloo.ca>
2025-03-20 15:47:12 -0400

    added example path

--- NavigationSolution/app/src/main/java/com/example/navigationsolution/IndoorMapActivity.kt
+++ NavigationSolution/app/src/main/java/com/example/navigationsolution/IndoorMapActivity.kt
@@ -418,7 +418,7 @@ fun IndoorMap(indoorViewModel: IndoorViewModel
     if (from != to || buildingFrom != buildingTo) {
         markedPlan = MapRepository.getMarkedPlan(buildingFrom, from, buildingTo, to)
     }
-    // markedPlan = MapRepository.getMarkedPlan(1, 1331, 2, 1004)
+    markedPlan = MapRepository.getMarkedPlan(1, 1331, 2, 1004)
     indoorViewModel.updatePath(newMaxFloor = markedPlan.size - 1)
     val floor = indoorViewModel.liveFloor.observeAsState(initial = 0)
 
